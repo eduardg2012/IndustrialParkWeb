af0770513c2a52550c0b7fdde9e10f2ca6d5bd61
eduardg2012 <35940088+eduardg2012@users.noreply.github.com>
2020-06-18 23:58:10 -0500

    Agregar archivos de proyecto.

---
+++ IndustrialParkWeb/Models/APIModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IndustrialParkWeb.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Codigo { get; set; }        
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+    }
+}
